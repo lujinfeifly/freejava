56c2497acf3273f883c8eea9792760b9dbc34464
Lu Jinfei <lujinfeifly@aliyun.com>
2016-08-15 11:03:26 +0800

    距离的计算

--- src/main/java/deer/milu/freejava/basic/MGpsUtil.java
+++ src/main/java/deer/milu/freejava/basic/MGpsUtil.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 public class MGpsUtil {
 
+	private static double EARTH_RADIUS = 6378.137;   //地球半径
 	private static char[] base32 = { '0', '1', '2', '3', '4', '5', '6', '7'};
 	private final static Map<Character, Integer> decodemap = new HashMap<Character, Integer>();
 	static {
@@ -121,7 +122,45 @@ public class MGpsUtil {
 		return new double[] { latitude, longitude, lat_err, lon_err };
 	}
 
+	private static double rad(double d) {
+		return d * Math.PI / 180.0;
+	} 
+	
+	public static double distanceFromGps(String gs, double lng, double lat) {
+		MGpsUtil ghf = new MGpsUtil();
+		double[] gd1 = ghf.decode(gs);
+		
+		double radLat1 = rad(lat); 
+		double radLat2 = rad(gd1[0]); 
+ 
+		double radLon1 = rad(lng); 
+		double radLon2 = rad(gd1[1]);
+	
+		if (radLat1 < 0) 
+			radLat1 = Math.PI / 2 + Math.abs(radLat1);// south  
+		if (radLat1 > 0) 
+			radLat1 = Math.PI / 2 - Math.abs(radLat1);// north  
+		if (radLon1 < 0) 
+			radLon1 = Math.PI * 2 - Math.abs(radLon1);// west  
+		if (radLat2 < 0)	
+			radLat2 = Math.PI / 2 + Math.abs(radLat2);// south  
+		if (radLat2 > 0) 
+			radLat2 = Math.PI / 2 - Math.abs(radLat2);// north  
+		if (radLon2 < 0) 
+			radLon2 = Math.PI * 2 - Math.abs(radLon2);// west  
+		double x1 = EARTH_RADIUS * Math.cos(radLon1) * Math.sin(radLat1); 
+		double y1 = EARTH_RADIUS * Math.sin(radLon1) * Math.sin(radLat1); 
+		double z1 = EARTH_RADIUS * Math.cos(radLat1); 
+		double x2 = EARTH_RADIUS * Math.cos(radLon2) * Math.sin(radLat2); 
+		double y2 = EARTH_RADIUS * Math.sin(radLon2) * Math.sin(radLat2); 
+		double z2 = EARTH_RADIUS * Math.cos(radLat2); 
 
+		double d = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)+ (z1 - z2) * (z1 - z2)); 
+		//余弦定理求夹角  
+		double theta = Math.acos((EARTH_RADIUS * EARTH_RADIUS + EARTH_RADIUS * EARTH_RADIUS - d * d) / (2 * EARTH_RADIUS * EARTH_RADIUS)); 
+		double dist = theta * EARTH_RADIUS; 
+		return dist; 
+	}
 	
 	public static void main(String[] args) {
 		MGpsUtil ghf = new MGpsUtil();
@@ -139,11 +178,14 @@ public class MGpsUtil {
 		System.out.println("天安门："+gc3);
 		System.out.println("东四环："+gc4);
 		System.out.println("济南哈："+gc6);
+		
+		double a = distanceFromGps("71644330065730", 116.211374, 39.913328);
 
 		double[] gd1 = ghf.decode(gc1);
 		double[] gd2 = ghf.decode(gc2);
 		System.out.println(gd1[0] + ", " + gd1[1]);
 		System.out.println(gd2[0] + ", " + gd2[1]);
+		System.out.println(a);
 	}
 
 }
