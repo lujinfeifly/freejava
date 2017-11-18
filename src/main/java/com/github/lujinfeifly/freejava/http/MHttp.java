8f90280726f97855404461b989d4b746161410b0
lujinfei <lujinfeifly@aliyun.com>
2017-11-19 00:13:44 +0800

    temp

--- src/main/java/com/github/lujinfeifly/freejava/http/MHttp.java
+++ src/main/java/com/github/lujinfeifly/freejava/http/MHttp.java
@@ -1,5 +1,6 @@
 package com.github.lujinfeifly.freejava.http;
 
+import com.github.lujinfeifly.freejava.basic.MString;
 import com.github.lujinfeifly.freejava.bean.MNameValuePair;
 
 import java.net.MalformedURLException;
@@ -40,13 +41,29 @@ public class MHttp {
 
         //
 
-        return MUrl.sendGetRequest(url, MUrl.paserList(param), sessionid, userAgent, timeout);
+        HttpRet ret = MUrl.sendGetRequest(url, MUrl.paserList(param), sessionid, userAgent, timeout);
 
+        if(MString.isNotEmpty(ret.getmSessionId())) {
+            cookieMap.put(urlKey, ret.getmSessionId());
+        }
+
+        return  ret;
     }
 
     public static void main(String[] args) {
-        HttpRet ret = getInstance().sendGetRequest("https://baidu.com", null);
+        HttpRet ret = getInstance().sendGetRequest("https://www.baidu.com", null);
         System.out.println(ret.getmSessionId());
+
+
+
+
+//        HttpRet ret2 = getInstance().sendPostRequest("https://dynamic.watch/users/sign_in", "utf8=%E2%9C%93&authenticity_token="+ java.net.URLEncoder.encode(bbb) +"&user%5Bemail%5D=736214397%40qq.com&user%5Bpassword%5D=lujinfei123.&user%5Bremember_me%5D=0&commit=Log+in",
+//                ret1.getmSessionId());
+//        System.out.println(ret2.toString());
+//
+//        HttpRet ret4 = sendGetRequest("http://dynamic.watch/me", "",
+//                ret2.getmSessionId());
+//        System.out.println(ret4.toString());
     }
 
 }
