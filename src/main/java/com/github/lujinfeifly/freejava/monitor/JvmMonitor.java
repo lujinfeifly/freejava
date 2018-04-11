2b4dc1d6a28b0cdb43088319266b1800f80eeb89
lujinfei <lujinfeifly@aliyun.com>
2018-04-12 00:04:12 +0800

    添加jvm的监控日志打印

---
+++ src/main/java/com/github/lujinfeifly/freejava/monitor/JvmMonitor.java
@@ -0,0 +1,47 @@
+package com.github.lujinfeifly.freejava.monitor;
+
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * Jvm 的监控类，目前该类为一个单独的线程。每隔一段时间进行收集JVM信息
+ *
+ */
+public class JvmMonitor implements InitializingBean{
+
+    private long heartbeatTime = 5*60*1000;
+
+    private Thread monitorThread;
+
+    private volatile boolean isStop = true;
+
+
+    private Runnable runnable = new Runnable() {
+        public void run() {
+            while(isStop) {
+                try {
+                    System.out.println("....");
+                    Thread.sleep(heartbeatTime);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    };
+
+
+    public void afterPropertiesSet() throws Exception {
+        if(monitorThread == null) {
+            monitorThread = new Thread(runnable);
+            monitorThread.setName("JvmMonitor thread.");
+            monitorThread.setDaemon(true);
+            monitorThread.start();
+        }
+    }
+
+    public long getHeartbeatTime() {
+        return heartbeatTime;
+    }
+
+    public void setHeartbeatTime(long heartbeatTime) {
+        this.heartbeatTime = heartbeatTime;
+    }
+}
